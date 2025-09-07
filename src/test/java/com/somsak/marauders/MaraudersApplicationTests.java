c2250f58f3cf0bd3e9face1558b0323fa3e69af2
somsak <somsak.kaeworasan@outlook.com>
2025-09-07 22:31:21 +0700

    Init project.

---
+++ src/test/java/com/somsak/marauders/MaraudersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somsak.marauders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaraudersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
